9413e71ca0771b8939d603b600de216653fa32f0
Kasturi Rangan Raghavan <kasturir@ucla.edu>
2010-10-14 02:10:27 -0700

    Rice DWT Library (rwt) tester updated

--- ecg/rwt_port/main.cpp
+++ ecg/rwt_port/main.cpp
@@ -1,3 +1,7 @@
+// Author: Kasturi Raghavan (kasturir@ucla.edu)
+// \file
+// Tests out dwt and idwt functions provided by the Rice
+// DWT package. The wavelet scaling coefficients must be provided
 #include <iostream>
 #include <cstdlib>
 #include "mdwt_r.h"
@@ -6,29 +10,32 @@
 using namespace std;
 
 int main() {
-
-
-	const int m = 4;
-	const int n = 1;
-	double scaling_coef[] = {0.4830, 0.8365, 0.2241, -0.1294};
+	const int m = 1;
+	const int n = 4;
 	const int scaling_coef_n = 4;
 	const int levels_n = 1;
-
+	const double scaling_coef[] = {0.4830, 0.8365, 0.2241, -0.1294};
+	const double signal_t[] = { 1.0, 1.0, 1.0, 1.0 };
+	
 	double *signal_dwt = (double*)calloc(m*n, sizeof(double));
 	double *signal_t1 = (double*)calloc(m*n, sizeof(double));
-	double signal_t[] = { 1.0, 1.0, 1.0, 1.0 };
 
-	MDWT(signal_t, m, n, scaling_coef, scaling_coef_n, levels_n, signal_dwt);
+	MDWT<m, n, scaling_coef_n, levels_n>(signal_t, scaling_coef, signal_dwt);
+	MIDWT<n, m, scaling_coef_n, levels_n>(signal_t1, scaling_coef, signal_dwt);
 
-	MIDWT(signal_t1, m, n, scaling_coef, scaling_coef_n, levels_n, signal_dwt);
+	cout << "Original signal: " << endl;
+	for (int i = 0; i < m*n; i++)
+		cout << signal_t[i] << endl;
 
+	cout << endl;
 
-	for(int i = 0; i < m*n; i++)
+	cout << "DWT of original signal: " << endl;
+	for (int i = 0; i < m*n; i++)
 		cout << signal_dwt[i] << endl;
 
 	cout << endl;
 
-	for(int i = 0; i < m*n; i++)
+	cout << "IDWT, recovered original signal: " << endl;
+	for (int i = 0; i < m*n; i++)
 		cout << signal_t1[i] << endl;
-
 }
